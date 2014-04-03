270d013fcba7550cde9cb1939a3ab3fe9d593753
Kyle Harrigan <kwharrigan@gmail.com>
2014-04-03 05:39:38 +0000

    Attempt to copy files to new job location.  Symlinks aren't relative so that doesn't work yet

--- run_gzip_restart.sh
+++ run_gzip_restart.sh
@@ -1,12 +1,11 @@
 PREV_JOB=$1
 DATARATE=$2
-#export TESTNAME="$JOB_ID"
-#workdir="/data/results/$JOB_ID"
-
-workdir="/data/results/$PREV_JOB/ckpt"
-cd $workdir
-env=$workdir/dmtcp_env.txt
-cp /data/dmtcp_env_template.txt $env
+new_workdir="/data/results/$JOB_ID/ckpt"
+prev_workdir="/data/results/$PREV_JOB/ckpt"
+mkdir -p $new_workdir
+cp -Pr $prev_workdir/* $new_workdir
+env=$new_workdir/dmtcp_env.txt
+cp dmtcp_env_template.txt $env
 sed -i -e "s/__DATARATE__/$DATARATE/g" $env
 sed -i -e 's/__DELAY__/2ms/g' $env
-$workdir/dmtcp_restart_script.sh
+$new_workdir/dmtcp_restart_script.sh --ckptdir $new_workdir
